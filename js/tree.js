4e56e2f6607cd67283ec89db231a6e395c9c5cf3
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-11-30 05:46:38 +0000

    Now it works to dynamically create the tree when submit button pressed

--- js/tree.js
+++ js/tree.js
@@ -1,7 +1,19 @@
-simple_chart_config = {
-    chart : {
-        container : "#tree-simple"
-    },
+$(document).ready(function() {
+    $('#submit-btn').click(function() {
+        var simple_chart_config = {
+            chart: {
+                container: "#tree-simple"
+            },
+            nodeStructure: buildHuffmanTree($('#icon_prefix2').val())
+        };
+        new Treant(simple_chart_config);
+    });
+});
 
-    nodeStructure : buildHuffmanTree("testing huffman tree building")
-};
+// simple_chart_config = {
+//     chart : {
+//         container : "#tree-simple"
+//     },
+//
+//     nodeStructure : buildHuffmanTree("testing huffman tree building")
+// };
