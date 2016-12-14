181a302374c0c3106c1265dabd1137f05eaa9957
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-12-14 13:09:46 +0200

    Toolbar aligned now Tree appears with vis.js when submit button is hit

--- js/tree.js
+++ js/tree.js
@@ -5,26 +5,8 @@ var fs = require('fs');
 
 $(document).ready(function () {
     $('#submit-btn').click(function () {
-        // var simple_chart_config = {
-        //     chart: {
-        //         container: "#tree-simple",
-        //         siblingSeparation: 2,
-        //         connectors: {
-        //             type: "step"
-        //         },
-        //         animation: {
-        //             nodeSpeed: 200,
-        //             nodeAnimation: "easeInSine",
-        //             connectorsAnimation: "easeInSine"
-        //         }
-        //     },
-        //     nodeStructure: buildHuffmanTree($('#icon_prefix2').val())
-        // };
-        // new Treant(simple_chart_config);
-        // var charFrequencies = getCharFrequency($('#icon_prefix2').val());
-        // console.log(charFrequencies);
-        var nodes = buildHuffmanTree($('#icon_prefix2').val()).nodes;
-        var edges = buildHuffmanTree($('#icon_prefix2').val()).edges;
+        var nodes = buildHuffmanTree($('#inputText').val()).nodes;
+        var edges = buildHuffmanTree($('#inputText').val()).edges;
         
         // create a network
         var container = document.getElementById('tree-simple');
@@ -78,24 +60,24 @@ $(document).ready(function () {
     });
 });
 
-// document.getElementById('upload-btn').addEventListener('click', function () {
-//     dialog.showOpenDialog(function (fileNames) {
-//         if (fileNames === undefined) {
-//             console.log("No file selected");
-//         }
-//         else {
-//             document.getElementById("actual-file").value = fileNames[0];
-//             var text = readFile(fileNames[0]);
-//             var simple_chart_config = {
-//                 chart: {
-//                     container: "#tree-simple"
-//                 },
-//                 nodeStructure: buildHuffmanTree(text)
-//             };
-//             new Treant(simple_chart_config);
-//         }
-//     });
-// }, false);
+document.getElementById('upload-btn').addEventListener('click', function () {
+    dialog.showOpenDialog(function (fileNames) {
+        if (fileNames === undefined) {
+            console.log("No file selected");
+        }
+        else {
+            document.getElementById("actual-file").value = fileNames[0];
+            var text = readFile(fileNames[0]);
+            var simple_chart_config = {
+                chart: {
+                    container: "#tree-simple"
+                },
+                nodeStructure: buildHuffmanTree(text)
+            };
+            new Treant(simple_chart_config);
+        }
+    });
+}, false);
 
 function readFile(filepath) {
     fs.readFile(filepath, 'utf-8', function (err, data) {
