b5d238e94b9e002e9fbaa22f728cd81ba146ac6b
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-12-18 04:31:25 +0200

    Added status report for the project Built initial animation for tree

--- js/tree.js
+++ js/tree.js
@@ -18,38 +18,12 @@ $(document).ready(function () {
             if (fileNames === undefined) {
                 console.log("No file selected");
             } else {
-                document.getElementById("actual-file").value = fileNames[0];
                 var text = readFile(fileNames[0]);
-                var simple_chart_config = {
-                    chart: {
-                        container: "#tree-simple"
-                    },
-                    nodeStructure: buildHuffmanTree(text)
-                };
-                new Treant(simple_chart_config);
+                constructVisTree(text);
             }
         });
     });
 });
-
-// document.getElementById('upload-btn').addEventListener('click', function () {
-//     dialog.showOpenDialog(function (fileNames) {
-//         if (fileNames === undefined) {
-//             console.log("No file selected");
-//         }
-//         else {
-//             document.getElementById("actual-file").value = fileNames[0];
-//             var text = readFile(fileNames[0]);
-//             var simple_chart_config = {
-//                 chart: {
-//                     container: "#tree-simple"
-//                 },
-//                 nodeStructure: buildHuffmanTree(text)
-//             };
-//             new Treant(simple_chart_config);
-//         }
-//     });
-// }, false);
 
 function readFile(filepath) {
     fs.readFile(filepath, 'utf-8', function (err, data) {
@@ -83,8 +57,47 @@ function constructVisTree(text) {
                 parentCentralization: true,
                 sortMethod: "directed"
             }
+        },
+        physics: {
+            enabled: false
         }
     };
 
     var network = new Vis.Network(container, data, options);
+
+    var positions = network.getPositions();
+	
+	// animation properties
+	var k = 0, lambda = 0, tick = 10, totalTime = 5000;
+		
+	// toy example start x, y coordinates nodes
+	var x_start = 0, y_start = 0
+	// nr of steps, given tick time and total animation time
+	var nrOfSteps = Math.floor( totalTime / tick);
+	// perform moveNode every tick nr of milliseconds
+	timer = setInterval(function(){
+		
+		// iteration counter
+		k++;
+		// lambda (for convex combination)
+		var l = k / nrOfSteps;
+		for (i = 1; i < nodes.length; i++) { 
+		
+			// get target positions 
+			var x_target = positions[i].x;
+			var y_target = positions[i].y;
+			
+			// compute the convex combination of x_start and x_target to find intermediate x and move node to it, same for y
+			var xt = x_start * (1 - l) + x_target * l;
+			var yt = y_start * (1 - l) + y_target * l;
+			
+			// move node
+		    network.moveNode(i,xt,yt);
+		}
+	
+		// stop if we have reached nr of steps
+		if(k == nrOfSteps){
+			clearInterval(timer)
+		} 
+	},tick);
 }
