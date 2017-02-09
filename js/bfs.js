f96037b469902c77f0bc9651ae853e862fb97210
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-02-09 22:13:49 +0000

    Made the initial animation work

--- js/bfs.js
+++ js/bfs.js
@@ -8,9 +8,10 @@ var inc = 0;
 
 $(document).ready(function () {
     $('#submit-btn').click(function () {
-        var rootNode = network.body.nodes[Object.keys(network.body.nodes)[0]];
-        startBFS(rootNode);
-        startCodeLinesAnimation();
+	var data = network.body.data.nodes;
+	var rootNode = data.get()[0];
+	startBFS(rootNode);
+	startCodeLinesAnimation();
     });
 });
 
@@ -20,49 +21,49 @@ var container = $('#tree-simple')[0];
 var options = {
     autoResize: true,
     manipulation: {
-        enabled: true,
-        initiallyActive: true,
-        addNode: function(nodeData, callback) {
-            if (network.body.nodes === {}) {
-                inc = 1;
-            }
-            else {
-                inc++;
-            }
-            nodeData.label = inc;
-            nodeData.color = "#009688";
-            nodeData.font = {
-                color: "#fff"
-            };
-            nodeData.visited = false;
-            nodeData.adjacencyList = [];
-            nodeData.predecessor = null;
-            callback(nodeData);
-        },
-        editNode: function(nodeData, callback) {
-            nodeData.root = true;
-            nodeData.color = "#3f51b5";
-            callback(nodeData);
-        },
-        addEdge: function(edgeData,callback) {
-            network.body.nodes[edgeData.from].options.adjacencyList.push(
-                network.body.nodes[edgeData.to]);
-            if (edgeData.from === edgeData.to) {
-                var r = confirm("Do you want to connect the node to itself?");
-                if (r === true) {
-                    callback(edgeData);
-                }
-            }
-            else {
-                callback(edgeData);
-            }
-        }
+	enabled: true,
+	initiallyActive: true,
+	addNode: function (nodeData, callback) {
+	    if (network.body.nodes === {}) {
+		inc = 1;
+	    }
+	    else {
+		inc++;
+	    }
+	    nodeData.label = inc;
+	    nodeData.color = '#009688';
+	    nodeData.font = {
+		color: '#fff'
+	    };
+	    nodeData.visited = false;
+	    nodeData.adjacencyList = [];
+	    nodeData.predecessor = null;
+	    callback(nodeData);
+	},
+	editNode: function (nodeData, callback) {
+	    nodeData.root = true;
+	    nodeData.color = '#3f51b5';
+	    callback(nodeData);
+	},
+	addEdge: function (edgeData,callback) {
+	    network.body.nodes[edgeData.from].options.adjacencyList.push(
+		network.body.nodes[edgeData.to]);
+		if (edgeData.from === edgeData.to) {
+		    var r = confirm('Do you want to connect the node to itself?');
+		    if (r === true) {
+			callback(edgeData);
+		    }
+		}
+		else {
+		    callback(edgeData);
+		}
+	}
     },
     interaction: {
-        navigationButtons: true
+	navigationButtons: true
     },
     physics: {
-        enabled: false
+	enabled: false
     }
 };
 
@@ -70,69 +71,69 @@ network = new Vis.Network(container, [], options);
 
 function startBFS(root) {
     var queue = [];
-    console.log(root);
+    var nodes = network.body.data.nodes.get();
     (function () {
-        setTimeout(function () {
-            root.options.color = "white";
-            root.options.visited = true;
-            network = rebuildNetwork();
-        }, (1000));
+	setTimeout(function () {
+	    root.visited = true;
+	    root.color = "red";
+	    nodes[0] = root;
+	    network = rebuildNetwork(nodes);
+	}, (1000));
     })();
     (function () {
-        setTimeout(function () {
-            queue.push(root);
-            appendToQueue(root.options.label)
-        }, (2000));
+	setTimeout(function () {
+	    queue.push(root);
+	    appendToQueue(root.label)
+	}, (2000));
     })();
 
     while (queue.length > 0) {
-        var u = queue.pop();
-        var adjacencyList = u.options.adjacencyList;
-        for (var i = 0; i < adjacencyList.length; i++) {
-            if (!adjacencyList[i].options.visited) {
-                console.log("label: " + adjacencyList[i].options.label);
-                adjacencyList[i].options.visited = true;
-                adjacencyList[i].options.predecessor = u;
-                queue.push(adjacencyList[i]);
-            }
-        }
+	var u = queue.pop();
+	var adjacencyList = u.adjacencyList;
+	for (var i = 0; i < adjacencyList.length; i++) {
+	    if (!adjacencyList[i].visited) {
+		console.log('label: ' + adjacencyList[i].label);
+		adjacencyList[i].visited = true;
+		adjacencyList[i].predecessor = u;
+		queue.push(adjacencyList[i]);
+	    }
+	}
     }
 }
 
 function startCodeLinesAnimation() {
     for (var index1 = 0; index1 < 3; index1++) {
-        (function (ind1) {
-            setTimeout(function () {
-                unHighlightCodeLine(ind1 - 1);
-                highlightCodeLine(ind1);
-            }, (1000 * ind1));
-        })(index1);
+	(function (ind1) {
+	    setTimeout(function () {
+		unHighlightCodeLine(ind1 - 1);
+		highlightCodeLine(ind1);
+	    }, (1000 * ind1));
+	})(index1);
     }
 }
 
 function highlightCodeLine(number) {
-    $("#line-" + number).css('color', 'red');
+    $('#line-' + number).css('color', 'red');
 }
 
 function unHighlightCodeLine(number) {
-    $("#line-" + number).css('color', '#3f51b5');
+    $('#line-' + number).css('color', '#3f51b5');
 }
 
-
 function appendToQueue(vertex, position) {
-    var queueID = "#queue-" + position;
+    var queueID = '#queue-' + position;
     $(queueID).text(vertex);
 }
 
 function createAlert(alertText) {
-    var alert = "<div id='customAlert' class='alert alert-dismissible alert-danger'> <button type='button' class='close' data-dismiss='alert'> x </button> <strong>Oh snap!</strong> " + alertText + " </div>";
+    var alert = "<div id='customAlert' class='alert alert-dismissible alert-danger'> <button type='button' class='close' data-dismiss='alert'> x </button> <strong>Oh snap!</strong> " + alertText + ' </div>';
     return alert;
 }
 
-function rebuildNetwork () {
+function rebuildNetwork(nodes) {
     var data = {
-        nodes: network.body.data.nodes,
-        edges: network.body.data.edges
+	nodes: nodes,
+	edges: network.body.data.edges
     };
 
     network.destroy();
