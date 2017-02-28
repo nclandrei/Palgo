2eae13af30f0c20d811badd8de0b16fc3adf5de7
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-02-28 21:30:47 +0000

    other duplication removal - cleaning up

--- js/dijkstra/dijkstra.js
+++ js/dijkstra/dijkstra.js
@@ -146,18 +146,3 @@ function setupDistances(nodes) {
         }
     }
 }
-
-function markAllNodesAsUnvisited(path) {
-    for (var i = 0; i < path.length; i++) {
-        path[i].visited = false;
-    }
-    return path;
-}
-
-function findRootNode(path) {
-    for (var i = 0; i < path.length; i++) {
-        if (path[i].root) {
-            return path[i];
-        }
-    }
-}
