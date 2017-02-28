b62d4a05c5ff4ad8f5cc7dd5df1d70c1951097d0
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-02-28 21:29:34 +0000

    removed unecessary code repetition in dijkstra

--- js/dijkstra/dijkstra.js
+++ js/dijkstra/dijkstra.js
@@ -168,20 +168,6 @@ function markAllNodesAsUnvisited(path) {
     return path;
 }
 
-function createAlert(alertText) {
-    var alert = "<div id='customAlert' class='alert alert-dismissible alert-danger'> <button type='button' class='close' data-dismiss='alert'> x </button> <strong>Oh snap!</strong> " + alertText + ' </div>';
-    $('#algo-panel').prepend(alert);
-}
-
-function alertUserThatNoRoot() {
-    var alert = "<div class='alert alert-dismissible alert-info'> \
-    <button type='button' class='close' data-dismiss='alert'>x</button> \
-    <strong>Heads up!</strong> You have not selected any \
-    root node, so the first node will be automatically set as root. \
-    </div>";
-    return alert;
-}
-
 function findRootNode(path) {
     for (var i = 0; i < path.length; i++) {
         if (path[i].root) {
