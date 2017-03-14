05748b81458ecd032c8525e3b052ad6d6099ce3f
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-14 15:44:48 +0000

    completed tv/ntv appending functions

--- js/prim-jarnik/prim-jarnik.js
+++ js/prim-jarnik/prim-jarnik.js
@@ -214,17 +214,17 @@ function getEdgeWeight(nodeOne, nodeTwo) {
 }
 
 function appendElementToTv(label) {
-    $("#tv-line").append("<td style='text-align: center'>" + label + "</td>");
+    $("#tv-line").append("<td style='text-align: center' id='tv-" + label + "'>" + label + "</td>");
 }
 
 function appendElementToNtv(label) {
-    $("#ntv-line").append("<td style='text-align: center'>" + label + "</td>");
+    $("#ntv-line").append("<td style='text-align: center' id='ntv-" + label + "'>" + label + "</td>");
 }
 
 function removeElementFromTv(label) {
-
+    $("#tv-" + label).remove();
 }
 
 function removeElementFromNtv(label) {
-
+    $("#ntv-" + label).remove();
 }
