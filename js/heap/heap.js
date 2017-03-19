76d749246c9037501587d51dcd609a3abd92cfa8
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-19 20:56:03 +0000

    corrected and refactored the skeleton for heap js

--- js/heap/heap.js
+++ js/heap/heap.js
@@ -7,24 +7,10 @@ var fs = require('fs');
 var network;
 
 $(document).ready(function () {
-
-    $('#submit-btn').click(function () {
-        if (network.body.data.nodes.get().length == 0) {
-            createAlert("You have not added any nodes to the graph.");
-            return;
-        }
-        if (network.body.data.edges.get().length == 0) {
-            createAlert("You have not added any edges to the graph.");
-            return;
-        }
-        var rootNode = findRootNode(network.body.data.nodes.get());
-        if (!rootNode) {
-            var nodes = network.body.data.nodes.get();
-            rootNode = nodes[0];
-            rootNode.root = true;
-            network = rebuildNetwork(network, container, options, nodes);
-            $("#algo-panel").prepend(alertUserThatNoRoot());
-        }
+    $('#insert-btn').click(function () {
+        primJarnikAnimation(network.body.data.nodes.get());
+    });
+    $('#delete-btn').click(function () {
         primJarnikAnimation(network.body.data.nodes.get());
     });
 
@@ -37,6 +23,7 @@ $(document).ready(function () {
         network = new Vis.Network(container, data, options);
     });
 });
+
 var container = $('#tree-simple')[0];
 
 var options = {
@@ -59,140 +46,14 @@ var options = {
 
 network = new Vis.Network(container, [], options);
 
-function primJarnikAnimation(nodes) {
-    var rIndex = Math.floor(Math.random() * nodes.length);
-    nodes[rIndex].tv = true;
-    appendElementToTv(nodes[rIndex].label);
+function insertItem() {
 
-    var nodesArrayLength = nodes.length;
-
-    var ntvSet = getNtvNodes(nodes);
-    var tvSet = [nodes[rIndex]];
-
-    for (var z = 0; z < nodesArrayLength; z++) {
-        if (!nodes[z].tv) {
-            appendElementToNtv(nodes[z].label);
-        }
-    }
-
-    nodes[rIndex].color = "red";
-    network = rebuildNetwork(network, container, options, nodes);
-    highlightCodeLine(0);
-
-    setTimeout(function() {
-        nodes[rIndex].color = "#3f51b5";
-        unHighlightCodeLine(0);
-        highlightCodeLine(1);
-        network = rebuildNetwork(network, container, options, nodes);
-    }, 1000);
-
-    for (var i = 0; i < nodesArrayLength - 1; i++) {
-        (function (ind) {
-            setTimeout(function() {
-                unHighlightAllCodeLines();
-                highlightCodeLine(2);
-                highlightCodeLine(3);
-                var minNodes = findMinWeightEdge(tvSet, ntvSet);
-                var p = minNodes.p;
-                var q = minNodes.q;
-
-                setTimeout(function() {
-                    highlightCodeLine(4);
-                    p.color = "red";
-                    network = rebuildNetwork(network, container, options, nodes);
-                }, 1000);
-
-                setTimeout(function() {
-                    unHighlightCodeLine(4);
-                    highlightCodeLine(5);
-                    q.color = "red";
-                    network = rebuildNetwork(network, container, options, nodes);
-                }, 2000);
-
-                setTimeout(function() {
-                    unHighlightCodeLine(5);
-                    highlightCodeLine(6);
-                }, 3000);
-
-                setTimeout(function() {
-                    unHighlightCodeLine(6);
-                    unHighlightCodeLine(3);
-                    highlightCodeLine(7);
-                    p.color = "#3f51b5";
-                    q.color = "#3f51b5";
-                    network = rebuildNetwork(network, container, options, nodes);
-                }, 4000);
-
-                setTimeout(function() {
-                    unHighlightCodeLine(7);
-                    highlightCodeLine(8);
-                    tvSet.push(q);
-                    appendElementToTv(q.label);
-                    removeElementFromNtv(q.label);
-                    ntvSet.splice(ntvSet.indexOf(q), 1);
-                }, 5000);
-            }, 2000 + 6000 * ind);
-        })(i);
-    }
-
-    setTimeout(function() {
-        unHighlightAllCodeLines();
-        resetWholeNetwork(network, container, options);
-    }, 2000 + (6000 * nodesArrayLength - 1));
 }
 
-function findMinWeightEdge (tvSet, ntvSet) {
-    var minEdgeNodes = {};
-    var minWeight = Number.MAX_VALUE;
-    for (var i = 0; i < tvSet.length; i++) {
-        for (var j = 0; j < ntvSet.length; j++) {
-            var weight;
-            if (containsObject(ntvSet[j], tvSet[i].adjacencyList)) {
-                weight = getEdgeWeight(tvSet[i], ntvSet[j]);
-                if (weight < minWeight) {
-                    minEdgeNodes.p = tvSet[i];
-                    minEdgeNodes.q = ntvSet[j];
-                    minWeight = weight;
-                }
-            }
-            else if (containsObject(tvSet[i], ntvSet[j].adjacencyList)) {
-                weight = getEdgeWeight(ntvSet[j], tvSet[i]);
-                if (weight < minWeight) {
-                    minEdgeNodes.p = tvSet[i];
-                    minEdgeNodes.q = ntvSet[j];
-                    minWeight = weight;
-                }
-            }
-        }
-    }
-    return minEdgeNodes;
+function deleteItem() {
+
 }
 
-function getNtvNodes(nodes) {
-    var set = [];
-    for (var i = 0; i < nodes.length; i++) {
-        if (!nodes[i].tv) {
-            set.push(nodes[i]);
-        }
-    }
-    return set;
-}
+function impose() {
 
-function getEdgeWeight(nodeOne, nodeTwo) {
-    var edgeBetweenNodes = network.body.data.edges.get().filter(function(x) {
-        return (x.from === nodeOne.id && x.to === nodeTwo.id);
-    });
-    return parseInt(edgeBetweenNodes[0].label);
-}
-
-function appendElementToTv(label) {
-    $("#tv-line").append("<td style='text-align: center' id='tv-" + label + "'>" + label + "</td>");
-}
-
-function appendElementToNtv(label) {
-    $("#ntv-line").append("<td style='text-align: center' id='ntv-" + label + "'>" + label + "</td>");
-}
-
-function removeElementFromNtv(label) {
-    $("#ntv-" + label).remove();
 }
