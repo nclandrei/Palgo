b72dce8d49b2f8f4318c983107619d548cc86b6b
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-04-03 16:09:19 +0300

    added test for huffman findSmallestTwoNodes

--- js/huffman/huffman-helper.js
+++ js/huffman/huffman-helper.js
@@ -1,170 +1,178 @@
-function Node() {
-    this.id = 0;
-    this.value = '';
-    this.children = [];
-    this.weight = 0;
-    this.parent = null;
-    this.label = "";
-    this.shape = "ellipse";
-    this.hidden = true;
-    this.color = "#009688";
-    this.font = {color : "#ffffff"};
-    this.edges = [];
+const huffman = {};
 
-    this.getEdges = function () {
-        return this.edges;
-    };
+huffman.Node = function() {
+  this.id = 0;
+  this.value = "";
+  this.children = [];
+  this.weight = 0;
+  this.parent = null;
+  this.label = "";
+  this.shape = "ellipse";
+  this.hidden = true;
+  this.color = "#009688";
+  this.font = { color: "#ffffff" };
+  this.edges = [];
 
-    this.addEdge = function (edge) {
-        this.edges.push(edge);
-    };
+  this.getEdges = function() {
+    return this.edges;
+  };
 
-    this.setShape = function (shape) {
-        this.shape = shape;
-    };
+  this.addEdge = function(edge) {
+    this.edges.push(edge);
+  };
 
-    this.setId = function (id) {
-        this.id = id;
-    };
+  this.setShape = function(shape) {
+    this.shape = shape;
+  };
 
-    this.getId = function () {
-        return this.id;
-    };
+  this.setId = function(id) {
+    this.id = id;
+  };
 
-    this.setParentNode = function (node) {
-        this.parent = node;
-    };
+  this.getId = function() {
+    return this.id;
+  };
 
-    this.getParentNode = function () {
-        return this.parent;
-    };
+  this.setParentNode = function(node) {
+    this.parent = node;
+  };
 
-    this.addChild = function (node) {
-        node.setParentNode(this);
-        this.children[this.children.length] = node;
-    };
+  this.getParentNode = function() {
+    return this.parent;
+  };
 
-    this.getWeight = function () {
-        return this.weight;
-    };
+  this.addChild = function(node) {
+    node.setParentNode(this);
+    this.children[this.children.length] = node;
+  };
 
-    this.setWeight = function (weight) {
-        this.weight = weight;
-    };
+  this.getWeight = function() {
+    return this.weight;
+  };
 
-    this.getValue = function () {
-        return this.value;
-    };
+  this.setWeight = function(weight) {
+    this.weight = weight;
+  };
 
-    this.setValue = function (value) {
-        this.value = value;
-    };
+  this.getValue = function() {
+    return this.value;
+  };
 
-    this.getLabel = function () {
-        return this.label;
-    };
+  this.setValue = function(value) {
+    this.value = value;
+  };
 
-    this.setLabel = function (label) {
-        this.label = label;
-    };
-}
+  this.getLabel = function() {
+    return this.label;
+  };
 
-function getCharFrequency(str) {
-    let freq = [];
-    for (let i = 0; i < str.length; i++) {
-        let char1 = str[i];
-        if (freq[char1]) {
-            freq[char1]++;
-        } else {
-            freq[char1] = 1;
-        }
+  this.setLabel = function(label) {
+    this.label = label;
+  };
+};
+
+huffman.getCharFrequency = function(str) {
+  let freq = [];
+  for (let i = 0; i < str.length; i++) {
+    let char1 = str[i];
+    if (freq[char1]) {
+      freq[char1]++;
+    } else {
+      freq[char1] = 1;
     }
-    return freq;
-}
+  }
+  return freq;
+};
 
-function buildHuffmanTree(str) {
-    let frequenciesSorted = getCharFrequency(str);
-    let uniqueCharString = str.split('').filter(function (item, i, ar) {
-        return ar.indexOf(item) === i;
-    }).join('');
-    let parentlessNodes = [];
-    let parentlessNode;
-    let visNodes = [];
-    let visEdges = [];
-    let i;
+huffman.buildHuffmanTree = function(str) {
+  let frequenciesSorted = huffman.getCharFrequency(str);
+  let uniqueCharString = str
+    .split("")
+    .filter(function(item, i, ar) {
+      return ar.indexOf(item) === i;
+    })
+    .join("");
+  let parentlessNodes = [];
+  let parentlessNode;
+  let visNodes = [];
+  let visEdges = [];
+  let i;
 
-    for (i = 0; i < uniqueCharString.length; i++) {
-        let node = new Node();
-        node.setValue(uniqueCharString[i]);
-        node.setWeight(frequenciesSorted[uniqueCharString[i]]);
-        node.setId(i);
-        node.setLabel(frequenciesSorted[uniqueCharString[i]] + " : " + uniqueCharString[i]);
-        parentlessNodes.push(node);
-        node.setShape("box");
-        visNodes.push(node);
-    }
+  for (i = 0; i < uniqueCharString.length; i++) {
+    let node = new Node();
+    node.setValue(uniqueCharString[i]);
+    node.setWeight(frequenciesSorted[uniqueCharString[i]]);
+    node.setId(i);
+    node.setLabel(
+      frequenciesSorted[uniqueCharString[i]] + " : " + uniqueCharString[i]
+    );
+    parentlessNodes.push(node);
+    node.setShape("box");
+    visNodes.push(node);
+  }
 
-    let index = i;
-    while (parentlessNodes.length > 1) {
-        parentlessNode = new Node();
-        parentlessNode.setId(index);
-        let smallestNodes = findSmallestTwoNodes(parentlessNodes);
-        let firstSmallest = smallestNodes.firstMin;
-        let secondSmallest = smallestNodes.secondMin;
-        let firstEdge = {
-            from: index,
-            to: firstSmallest.getId(),
-            hidden: true
-        };
-        let secondEdge = {
-            from: index,
-            to: secondSmallest.getId(),
-            hidden: true
-        };
-        visEdges.push(firstEdge);
-        visEdges.push(secondEdge);
-        parentlessNode.addEdge(firstEdge);
-        parentlessNode.addEdge(secondEdge);
-        parentlessNode.addChild(firstSmallest);
-        parentlessNode.addChild(secondSmallest);
-        let weight = firstSmallest.getWeight() + secondSmallest.getWeight();
-        parentlessNode.setLabel(weight);
-        parentlessNode.setWeight(weight);
-        parentlessNodes.push(parentlessNode);
-        visNodes.push(parentlessNode);
-        parentlessNodes.splice(parentlessNodes.indexOf(firstSmallest), 1);
-        parentlessNodes.splice(parentlessNodes.indexOf(secondSmallest), 1);
-        index++;
-    }
-    return {
-        nodes: visNodes,
-        edges: visEdges
+  let index = i;
+  while (parentlessNodes.length > 1) {
+    parentlessNode = new Node();
+    parentlessNode.setId(index);
+    let smallestNodes = findSmallestTwoNodes(parentlessNodes);
+    let firstSmallest = smallestNodes.firstMin;
+    let secondSmallest = smallestNodes.secondMin;
+    let firstEdge = {
+      from: index,
+      to: firstSmallest.getId(),
+      hidden: true
     };
-}
-
-function findSmallestTwoNodes(arr) {
-    let minOne;
-    let minTwo;
-
-    minOne = arr[0];
-    minTwo = arr[1];
-
-    if (minTwo.getWeight() < minOne.getWeight()) {
-        minOne = arr[1];
-        minTwo = arr[0];
-    }
-
-    for (let i = 2; i < arr.length; i++) {
-        if (arr[i].getWeight() < minOne.getWeight()) {
-            minTwo = minOne;
-            minOne = arr[i];
-        }
-        else if (arr[i].getWeight() < minTwo.getWeight()) {
-            minTwo = arr[i];
-        }
-    }
-    return {
-        firstMin: minOne,
-        secondMin: minTwo
+    let secondEdge = {
+      from: index,
+      to: secondSmallest.getId(),
+      hidden: true
     };
-}
+    visEdges.push(firstEdge);
+    visEdges.push(secondEdge);
+    parentlessNode.addEdge(firstEdge);
+    parentlessNode.addEdge(secondEdge);
+    parentlessNode.addChild(firstSmallest);
+    parentlessNode.addChild(secondSmallest);
+    let weight = firstSmallest.getWeight() + secondSmallest.getWeight();
+    parentlessNode.setLabel(weight);
+    parentlessNode.setWeight(weight);
+    parentlessNodes.push(parentlessNode);
+    visNodes.push(parentlessNode);
+    parentlessNodes.splice(parentlessNodes.indexOf(firstSmallest), 1);
+    parentlessNodes.splice(parentlessNodes.indexOf(secondSmallest), 1);
+    index++;
+  }
+  return {
+    nodes: visNodes,
+    edges: visEdges
+  };
+};
+
+huffman.findSmallestTwoNodes = function(arr) {
+  let minOne;
+  let minTwo;
+
+  minOne = arr[0];
+  minTwo = arr[1];
+
+  if (minTwo.getWeight() < minOne.getWeight()) {
+    minOne = arr[1];
+    minTwo = arr[0];
+  }
+
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i].getWeight() < minOne.getWeight()) {
+      minTwo = minOne;
+      minOne = arr[i];
+    } else if (arr[i].getWeight() < minTwo.getWeight()) {
+      minTwo = arr[i];
+    }
+  }
+  return {
+    firstMin: minOne,
+    secondMin: minTwo
+  };
+};
+
+module.exports = huffman;
---
+++ test/huffman/TestHuffmanHelper.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const huffman = require("../../js/huffman/huffman-helper.js");
+
+describe('huffman-helper', function(){
+    describe('#findSmallestTwoNodes()', function(){
+        it('should return two nodes with the smallest label values', function(){
+            let firstNode = new huffman.Node();
+            let secondNode = new huffman.Node();
+            let thirdNode = new huffman.Node();
+            firstNode.id = 0;
+            secondNode.id = 1;
+            thirdNode.id = 2;
+            firstNode.weight = -2;
+            secondNode.weight = -3;
+            thirdNode.weight = 5;
+            assert.deepEqual({firstMin : secondNode, secondMin : firstNode},
+                huffman.findSmallestTwoNodes([firstNode, secondNode, thirdNode]));
+        });
+    });
+});
