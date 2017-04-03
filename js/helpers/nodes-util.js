9d38005b8cc0d9d7fa6f7b9d4a5fe62b804eb6b5
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-04-03 16:34:18 +0300

    ecma6

--- js/helpers/nodes-util.js
+++ js/helpers/nodes-util.js
@@ -1,62 +1,62 @@
 function markAllNodesAsUnvisited(path) {
-    for (var i = 0; i < path.length; i++) {
-        path[i].visited = false;
-    }
-    return path;
+  for (let i = 0; i < path.length; i++) {
+    path[i].visited = false;
+  }
+  return path;
 }
 
 function findRootNode(path) {
-    for (var i = 0; i < path.length; i++) {
-        if (path[i].root) {
-            return path[i];
-        }
+  for (let i = 0; i < path.length; i++) {
+    if (path[i].root) {
+      return path[i];
     }
+  }
 }
 
 function HeapNode() {
-    this.id = 0;
-    this.root = false;
-    this.children = [];
-    this.parent = null;
-    this.label = "";
-    this.hidden = false;
-    this.color = "#009688";
-    this.font = {color : "#ffffff"};
-    this.edges = [];
+  this.id = 0;
+  this.root = false;
+  this.children = [];
+  this.parent = null;
+  this.label = "";
+  this.hidden = false;
+  this.color = "#009688";
+  this.font = { color: "#ffffff" };
+  this.edges = [];
 
-    this.getEdges = function () {
-        return this.edges;
-    };
+  this.getEdges = function() {
+    return this.edges;
+  };
 
-    this.addEdge = function (edge) {
-        this.edges.push(edge);
-    };
+  this.addEdge = function(edge) {
+    this.edges.push(edge);
+  };
 
-    this.setId = function (id) {
-        this.id = id;
-    };
+  this.setId = function(id) {
+    this.id = id;
+  };
 
-    this.getId = function () {
-        return this.id;
-    };
+  this.getId = function() {
+    return this.id;
+  };
 
-    this.setParentNode = function (node) {
-        this.parent = node;
-    };
+  this.setParentNode = function(node) {
+    this.parent = node;
+  };
 
-    this.getParentNode = function () {
-        return this.parent;
-    };
+  this.getParentNode = function() {
+    return this.parent;
+  };
 
-    this.addChild = function (node) {
-        this.children.push(node);
-    };
+  this.addChild = function(node) {
+    this.children.push(node);
+  };
 
-    this.getLabel = function () {
-        return this.label;
-    };
+  this.getLabel = function() {
+    return this.label;
+  };
 
-    this.setLabel = function (label) {
-        this.label = label;
-    };
+  this.setLabel = function(label) {
+    this.label = label;
+  };
 }
